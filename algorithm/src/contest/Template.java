b65b2f64e8a2b13b9586b8a288b2d658e9895b75
totalo <totalo@qq.com>
2024-09-21 21:44:34 +0800

    feat: contest java template

--- algorithm/src/contest/Template.java
+++ algorithm/src/contest/Template.java
@@ -1,15 +1,17 @@
-package contest.atcoder.abc_b372;
-
+package contest;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
-public class B {
+/**
+ * java 提交模板
+ * atcoder、nowcoder 需要修改类名为 Main,并且去掉 package 语句
+ * codeforces 只需要去掉包名
+ */
+public class Template {
     
     static BufferedReader reader;
     static BufferedWriter writer;
@@ -18,36 +20,17 @@ public class B {
         reader = new BufferedReader(new InputStreamReader(System.in));
         writer = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int m = sc.nextInt();
-        List<Integer> ans = new ArrayList<>();
-        int[] pows = new int[11];
-        for (int i = 0; i <= 10; i++) {
-            pows[i] = (int) Math.pow(3, i);
-        }
-        int i = 0;
-        for (i = 1; i <= 20; i++) {
-            for (int j = 10; j >= 0; j--) {
-                int power = pows[j];
-                if (m >= power) {
-                    ans.add(j);
-                    m -= power;
-                    break;
-                }
-            }
-            
-            if (m == 0) {
-                break;
-            }
-        }
+        String s = sc.next();
         
-        writer.write(i + "\n");
-        for (i = ans.size() - 1; i >= 0; i--) {
-            writer.write(ans.get(i) + "");
-            if (i > 0) {
-                writer.write(" ");
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '.') {
+                continue;
             }
+            ans.append(c);
         }
-        writer.write("\n");
+        writer.write(ans + "\n");
         
         reader.close();
         writer.close();
